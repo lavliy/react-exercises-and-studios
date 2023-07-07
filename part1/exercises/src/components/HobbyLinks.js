e06d8decc163c66621ec589be29077033bcaff57
lavliy <natarajanlavanya92@gmail.com>
2023-07-07 01:56:58 -0500

    part2 & 3 completed

---
+++ part1/exercises/src/components/HobbyLinks.js
@@ -0,0 +1,14 @@
+export default function HobbyLinks(){
+    let hobbyLinks = ["https://open.spotify.com/", "https://cookdtv.com/"]
+
+    return(
+        <div>
+            <h3>{HobbyLinks}</h3>
+            <ol>
+                <li><a href ={hobbyLinks[0]}>Spotify for listening music</a></li>
+                <li><a href = {hobbyLinks[1]}>cookdtv for making good food</a></li>
+            </ol>
+        </div>
+    )
+
+}
