333f1abac6103b98bde715e23fd2186b819e68a2
lavliy <natarajanlavanya92@gmail.com>
2023-07-26 15:33:53 -0500

    in progress

--- part3/studio/src/components/Recipe.js
+++ part3/studio/src/components/Recipe.js
@@ -1,7 +1,7 @@
 const RecipeAuthor = () => {
-   let authorLink = "";
-   let authorPhoto = "";
-   let authorName = "";
+   let authorLink = "https://www.thechunkychef.com/family-favorite-baked-mac-and-cheese/";
+   let authorPhoto = "https://www.thechunkychef.com/wp-content/uploads/2021/02/sidebar-440x259.jpg";
+   let authorName = "Amanda";
 
    return (
       <div>
@@ -15,7 +15,7 @@ const RecipeAuthor = () => {
 }
 
 const RecipeIngredients = () => {
-   const ingredients = [];
+   const ingredients = ["Butter", "Flour", "Pasta", "Cheese", "Milk"];
    return(
       <div>
          <h3>Recipe Ingredients</h3>
@@ -34,8 +34,15 @@ const RecipeDescription = () => {
    return (
       <div> 
          <div>
-            <h1></h1>
-            <p></p>
+            <h1>Description</h1>
+            <p>Boil the pasta, drain.
+Make the cheese sauce by combining a fat (butter), and starch (flour), then whisking in the milk products.
+Cook the sauce until it’s nice and thick.
+Add in shredded cheeses, stir well.
+Combine cheese sauce with cooked pasta.
+Transfer half of the mac and cheese to a baking dish, sprinkle with more shredded cheese.
+Top with the rest of the mac and cheese, top with more shredded cheese.
+Bake and prepare to thoroughly enjoy!</p>
          </div>
          <div className="recipePhotoBlock">
             <RecipeIngredients />
@@ -47,7 +54,7 @@ const RecipeDescription = () => {
 
 const RecipePhoto = () => {
    return (
-      <img src="" alt="" className="imageUpdates"/>
+      <img src="https://www.thechunkychef.com/wp-content/uploads/2018/02/Ultimate-Creamy-Baked-Mac-and-Cheese-serving-680x1020.jpg" alt="Mac and cheese" className="imageUpdates"/>
    );
 }
 
